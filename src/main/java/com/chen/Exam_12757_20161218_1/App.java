5b1c60e92a5a30bbbac390dcdae31484d6457583
notsaltedfish <c1097210504@qq.com>
2016-12-18 16:20:55 +0800

    "第一次提交"

---
+++ src/main/java/com/chen/Exam_12757_20161218_1/App.java
@@ -0,0 +1,36 @@
+package com.chen.Exam_12757_20161218_1;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int count=0;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+    	for(int i=101;i<=200;i++){
+    		int j=2;
+    		boolean isPrimeNumber = true ;
+    		while(j<i){
+    			if(i%j==0){
+    				isPrimeNumber=false;
+    				break;
+    			}
+    			j++;
+    		}
+    		if(isPrimeNumber){
+    			numbers.add(i);
+    		}
+    	}
+    	
+    	System.out.print("101-200间一共有"+numbers.size()+"素数，分别是：");
+        for(int i:numbers){
+        	System.out.print(i+",");
+        }
+    }
+}
